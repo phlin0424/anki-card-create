d068d41bde81400f7ceb4005ebab4835c58509cb
PH <phlin@example.com>
2024-07-02 08:48:57 +0900

    slight fixings

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 DIR_PATH = Path(__file__).resolve().parent.parent
 API_URL = "http://127.0.0.1:8765"
@@ -30,6 +31,7 @@ class Config(BaseSettings):
         default="Basic (裏表反転カード付き)+sentense",
         description="The model name of the card being sent to Anki.",
     )
+    model_config = ConfigDict(protected_namespaces=("settings_",))
 
 
 settings = Config()
